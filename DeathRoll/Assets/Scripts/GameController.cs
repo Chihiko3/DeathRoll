cbaa8dcfb69e85f4bf801ff0981975e42af5f781
Chihiko <89134046+Chihiko3@users.noreply.github.com>
2022-01-26 18:53:35 +0800

    Almost finished
    Still need some SFX and Animation maybe

--- DeathRoll/Assets/Scripts/GameController.cs
+++ DeathRoll/Assets/Scripts/GameController.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI TextMeshProUGUI;
+    [SerializeField] private Text Text;
+    [SerializeField] private Text TextShadow;
+    [SerializeField] private GameObject RestartButton;
+    [SerializeField] private GameObject RollButton;
+
 
     public int ceiling;
     private int latestScore;
+    private bool isReadyToRestart = false;
     public void Start()
     {
-        ceiling = 100;
-        TextMeshProUGUI.text = ceiling.ToString();
+        RestartButton.SetActive(false);
+        RollButton.SetActive(true);
+    ceiling = 100;
+        Text.text = ceiling.ToString();
+        TextShadow.text = ceiling.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isReadyToRestart == true && ceiling == 1)
+            {
+                RestartScene();
+            }
+            else
+            {
+                DeathRoll();
+            }
+        }
     }
     public void DeathRoll()
     {
@@ -21,11 +44,28 @@ public class GameController : MonoBehaviour
             ceiling = latestScore;
         if (ceiling == 1)
         {
-            TextMeshProUGUI.text = "1!!!!";
+            Text.text = "1!!!!";
+            TextShadow.text = "1!!!!";
+            RollButton.SetActive(false);
+
+            StartCoroutine(Wait());
         }
         else
         {
-            TextMeshProUGUI.text = latestScore.ToString();
+            Text.text = latestScore.ToString();
+            TextShadow.text = latestScore.ToString();
         }
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.75f);
+        RestartButton.SetActive(true);
+        isReadyToRestart = true;
     }
 }
