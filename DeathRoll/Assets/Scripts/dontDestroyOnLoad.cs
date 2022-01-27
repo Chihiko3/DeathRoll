05a52567196cc9396e17d163e128831ba5bec236
Chihiko <89134046+Chihiko3@users.noreply.github.com>
2022-01-27 11:37:55 +0800

    Added audio when click roll

---
+++ DeathRoll/Assets/Scripts/dontDestroyOnLoad.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class dontDestroyOnLoad : MonoBehaviour
+{
+
+    private GameObject[] music;
+    void Start()
+    {
+        music = GameObject.FindGameObjectsWithTag("gameMusic");
+        if (music.Length != 1)
+        {
+            Destroy(music[1]);
+        }
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
+
